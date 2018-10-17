363b2a59db4efa99f4af353122bcaad882977942
Gabriella Torres <gabrielatorres@Interapts-MacBook-Pro-6.local>
2018-10-17 12:19:03 -0500

    Initial Commit

---
+++ Basic iOS Assessment 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Basic iOS Assessment 1
+//
+//  Created by Gabriela Torres on 10/17/18.
+//  Copyright © 2018 Gabriela Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
